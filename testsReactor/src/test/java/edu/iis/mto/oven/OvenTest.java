6ffeccec8854c988bed1019fad4a96f96b3a9635
Stasiak.Jacek <222579@edu.p.lodz.pl>
2021-05-14 16:57:46 +0200

    Implemented first test

--- testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
+++ testsReactor/src/test/java/edu/iis/mto/oven/OvenTest.java
@@ -1,42 +1,69 @@
 package edu.iis.mto.oven;
 
+import static edu.iis.mto.oven.Oven.HEAT_UP_AND_FINISH_SETTING_TIME;
+import static org.hamcrest.Matchers.any;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class OvenTest {
+    @Mock
+    private HeatingModule heating_module;
+    @Mock
+    private Fan fan;
+    private Oven oven;
 
     @Test
     void itCompiles() {
         MatcherAssert.assertThat(true, equalTo(true));
     }
 
+    @BeforeEach
+    void setUp() {
+        oven = new Oven(heating_module, fan);
+    }
+
     @Test
-    void runningOvenWithHeatingStageInProgram_shouldCompleteWithoutExceptions(){
+    void runningOvenWithHeatingStageInProgram_shouldCompleteWithoutExceptions() {
+        ProgramStage stage = ProgramStage.builder().withHeat(HeatType.HEATER).build();
+        BakingProgram program = BakingProgram.builder()
+                .withStages(List.of(stage))
+                .build();
+
+        oven.start(program);
+
+        verify(heating_module).heater(any());
+    }
+
+    @Test
+    void runningOvenWithGrillStageInProgram_shouldCompleteWithoutExceptions() {
 
     }
 
     @Test
-    void runningOvenWithGrillStageInProgram_shouldCompleteWithoutExceptions(){
+    void runningOvenWithInitialTemperatureAndHeatingStageInProgram_shouldCompleteWithoutExceptions() {
 
     }
 
     @Test
-    void runningOvenWithInitialTemperatureAndHeatingStageInProgram_shouldCompleteWithoutExceptions(){
+    void runningOvenWithThermoCirculationStageInProgram_shouldCompleteWithoutExceptions() {
 
     }
 
     @Test
-    void runningOvenWithThermoCirculationStageInProgram_shouldCompleteWithoutExceptions(){
-
-    }
-
-    @Test
-    void runningOvenWithThermoCirculationStageInProgram_HeatingModuleThrowsException_ovenShouldThrow_OvenException(){
+    void runningOvenWithThermoCirculationStageInProgram_HeatingModuleThrowsException_ovenShouldThrow_OvenException() {
 
     }
 }
